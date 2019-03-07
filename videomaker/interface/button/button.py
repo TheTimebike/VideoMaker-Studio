167880897175391017c0b872aaef694ab409c113
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-07 10:38:00 +0000

    changed function target to start thread

--- videomaker/interface/button/button.py
+++ videomaker/interface/button/button.py
@@ -1,8 +1,8 @@
 from tkinter import *
-from videomaker.functions.packageData import packageData
+from videomaker.functions.startThread import startThread
 
 def initButton(focus):
-    focus.startButton = Button(focus.master, text="Start!", width=42, height=5, command= lambda: packageData(focus))
+    focus.startButton = Button(focus.master, text="Start!", width=42, height=5, command= lambda: startThread(focus))
     # Cant make the text bigger or bolder without distorting the size of the button, even if the button is larger, the text is still
     # proportionate with the button
     focus.startButton.place(x=750, y=400)

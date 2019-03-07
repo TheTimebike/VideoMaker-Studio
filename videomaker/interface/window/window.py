9802697b7f8b8fa5d53f2d01d081a18fab0114b9
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-07 10:42:40 +0000

    moved misplaced function

--- videomaker/interface/window/window.py
+++ videomaker/interface/window/window.py
@@ -1,9 +1,1 @@
 
-from tkinter import *
-
-def initListboxes(focus):
-    focus.logBoxScrollbar = Scrollbar(focus.master, orient=VERTICAL)
-    focus.logBox = Listbox(focus.master, width=50, height=22, yscrollcommand=focus.logBoxScrollbar.set)  
-    focus.logBoxScrollbar.config(command=focus.logBox.yview) # Scrollbar is not showing up due to a bug, will be fixed however!
-    # if wraparound text is not possible, a horizontal oriented scrollbar might be added?
-    focus.logBox.place(x=750, y=35)

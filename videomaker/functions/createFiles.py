9ec03edda0fd5bf8f121ae24ad76a902cb85f0bf
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-07 10:32:18 +0000

    added support for creating praw.ini files

--- videomaker/functions/createFiles.py
+++ videomaker/functions/createFiles.py
@@ -1,7 +1,32 @@
 import sys, os
 
-def createFiles():
+def createDirectories():
     dirList = ["Downloaded Videos", "Audio", "Renders"]
     for pathName in dirList:
         if not os.path.exists("./"+pathName+"/"):
             os.mkdir("./"+pathName+"/")
+            
+def createFiles():
+    if not os.path.isfile("./praw.ini"):
+        fileString = """
+[DEFAULT]
+# A boolean to indicate whether or not to check for package updates.
+check_for_updates=True
+
+# Object to kind mappings
+comment_kind=t1
+message_kind=t4
+redditor_kind=t2
+submission_kind=t3
+subreddit_kind=t5
+
+# The URL prefix for OAuth-related requests.
+oauth_url=https://oauth.reddit.com
+
+# The URL prefix for regular requests.
+reddit_url=https://www.reddit.com
+
+# The URL prefix for short URLs.
+short_url=https://redd.it"""
+        with open("praw.ini", "w+") as out:
+            out.write(fileString)

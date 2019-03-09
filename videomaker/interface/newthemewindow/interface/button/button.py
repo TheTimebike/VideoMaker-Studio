83d7c57e4c20c448904821f8503c7ee0f9788242
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-09 17:04:35 +0000

    added buttons, awaiting command addition

--- videomaker/interface/newthemewindow/interface/button/button.py
+++ videomaker/interface/newthemewindow/interface/button/button.py
@@ -1,4 +1,11 @@
 from tkinter import *
 
 def initButton(focus):
-    pass
+    focus.selectThemeBackgroundColourButton = Button(focus.master, text="Select", width=10, height=1)
+    focus.selectThemeBackgroundColourButton.place(x=225, y=83)
+
+    focus.selectThemeBoxColourButton = Button(focus.master, text="Select", width=10, height=1)
+    focus.selectThemeBoxColourButton.place(x=225, y=133)
+
+    focus.selectThemeTextColourButton = Button(focus.master, text="Select", width=10, height=1)
+    focus.selectThemeTextColourButton.place(x=225, y=183)

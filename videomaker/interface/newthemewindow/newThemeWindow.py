759f4eab3dec3ad8231f1749ff0589be95fcc597
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 10:10:41 +0000

    added default theme based off current mainwindow theme

--- videomaker/interface/newthemewindow/newThemeWindow.py
+++ videomaker/interface/newthemewindow/newThemeWindow.py
@@ -1,15 +1,18 @@
 from tkinter import *
 from videomaker.interface.newthemewindow.interface.initializer import initWindow as oInitWindow
+from videomaker.interface.newthemewindow.interface.functions.changeTheme import changeTheme
 
 class Window(Frame):
     def __init__(self, master=None, main=None):
         Frame.__init__(self, master)
         self.master.title("Create A Theme")
-        oInitWindow(self)
         self.mainWindow = main
         self.chosenBackgroundColour = ""
         self.chosenBoxColour = ""
         self.chosenTextColour = ""
+        oInitWindow(self)
+        changeTheme(self)
+        
 
 def initWindow(mainWindow):
     root = Tk()

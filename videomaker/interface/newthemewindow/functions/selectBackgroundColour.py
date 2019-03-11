d9f55a638f4ab80c4c012f885ad8567f8bb3272b
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 09:58:48 +0000

    Update selectBackgroundColour.py

--- videomaker/interface/newthemewindow/functions/selectBackgroundColour.py
+++ videomaker/interface/newthemewindow/functions/selectBackgroundColour.py
@@ -3,3 +3,5 @@ from tkinter.colorchoser import askcolor as askcolour
 
 def askBackgroundColour(focus):
     focus.chosenBackgroundColour = askcolour()
+    stringVar = StringVar(focus.chosenBackgroundColour[1])
+    focus.selectThemeBackgroundColourBox.set(stringVar)

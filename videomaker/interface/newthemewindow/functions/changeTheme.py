42941c57665007d4a3690e04ac18b379bc72c2e7
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 10:09:19 +0000

    added subwindow theme changing functionality
    based on last stored theme variables in mainwindow

--- videomaker/interface/newthemewindow/functions/changeTheme.py
+++ videomaker/interface/newthemewindow/functions/changeTheme.py
@@ -1,1 +1,16 @@
-
+def changeTheme(focus):
+    focus.backgroundColour = focus.main.backgroundColour
+    focus.boxColour = focus.main.boxColour
+    focus.textColour = focus.main.textColour
+    
+    focus.master.configure(background=focus.backgroundColour)
+    
+    focus.selectThemeNameBox.configure(background=focus.boxColour)
+    focus.selectThemeBackgroundColourBox.configure(background=focus.boxColour)
+    focus.selectThemeTextColourBox.configure(background=focus.boxColour)
+    focus.selectThemeBoxColourBox.configure(background=focus.boxColour)
+    
+    focus.selectThemeNameBoxLabel.configure(background=focus.backgroundColour, foreground=focus.textColour)
+    focus.selectThemeBackgroundColourBoxLabel.configure(background=focus.backgroundColour, foreground=focus.textColour))
+    focus.selectThemeTextColourBoxLabel.configure(background=focus.backgroundColour, foreground=focus.textColour)
+    focus.selectThemeBoxColourBoxLabel.configure(background=focus.backgroundColour, foreground=focus.textColour)

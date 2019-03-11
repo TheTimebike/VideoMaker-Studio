9fbce6d63982a7981bedc4bba6840a0a97bfb24a
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 10:05:59 +0000

    added mainwindow as class variable

--- videomaker/interface/newthemewindow/newThemeWindow.py
+++ videomaker/interface/newthemewindow/newThemeWindow.py
@@ -2,16 +2,17 @@ from tkinter import *
 from videomaker.interface.newthemewindow.interface.initializer import initWindow as oInitWindow
 
 class Window(Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=None, main=None):
         Frame.__init__(self, master)
         self.master.title("Create A Theme")
         oInitWindow(self)
+        self.mainWindow = main
         self.chosenBackgroundColour = ""
         self.chosenBoxColour = ""
         self.chosenTextColour = ""
 
-def initWindow():
+def initWindow(mainWindow):
     root = Tk()
     root.geometry("400x400")
-    apps = Window(root)
+    apps = Window(root, main=mainWindow)
     root.mainloop()

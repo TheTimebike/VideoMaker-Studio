25c5f96f3a5a4d235633368ed87acb7881c57f4a
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 11:46:48 +0000

    Update startFromFile.py

--- videomaker/functions/startFromFile.py
+++ videomaker/functions/startFromFile.py
@@ -1,1 +1,9 @@
+import json, threading
+from videomaker.functions.verifyData import verifyData
 
+def startFromFile(focus, path):
+    with open(path, "r") as out:
+        fileData = json.load(out)
+    focus.dataPackage = fileData
+    startFromFileThread = threading.Thread(target=verifyData)
+    startFromFileThread.start()

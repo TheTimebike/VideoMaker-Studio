d0da3bc56bda1665bd39fb85897bc14b8b0a3766
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 13:35:46 +0000

    Update savePreset.py

--- videomaker/functions/savePreset.py
+++ videomaker/functions/savePreset.py
@@ -1,1 +1,7 @@
+import json
+from videomaker.functions.packageData import packageData
 
+def savePreset(focus):
+    preset = packageData(focus, continue=False)
+    with open(preset["subredditName"], "w+") as out:
+        json.dump(preset, out, indent=4)

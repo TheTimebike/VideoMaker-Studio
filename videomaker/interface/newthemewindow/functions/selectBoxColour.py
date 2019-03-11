03f145e6ee8897da37d9893efcad84ef2ae451df
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-11 09:59:21 +0000

    Update selectBoxColour.py

--- videomaker/interface/newthemewindow/functions/selectBoxColour.py
+++ videomaker/interface/newthemewindow/functions/selectBoxColour.py
@@ -3,3 +3,5 @@ from tkinter.colorchoser import askcolor as askcolour
 
 def askBoxColour(focus):
     focus.chosenBoxColour = askcolour()
+    stringVar = StringVar(focus.chosenBoxColour[1])
+    focus.selectThemeBoxColourBox.set(stringVar)

1f675ec76dac95182208ca10deaaac919b8472ba
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-12 14:31:53 +0000

    Update addPreset.py

--- videomaker/functions/addPreset.py
+++ videomaker/functions/addPreset.py
@@ -1,18 +1,13 @@
-from videomaker.functions.verifyData import verifyData
+from videomaker.functions.startFromFile import startFromFile
 import glob, os, threading
 
-def addOption(focus, preset, name):
-    focus.menuDropdownStartFromFile.add_command(label="Start {0} Preset".format(name), command= lambda: startThread(focus, preset))
-    
-def startThread(focus, preset):
-    threading.Thread(target=verifyData, args=(focus,preset)).start()
+def addOption(focus, path, name):
+    focus.menuDropdownStartFromFile.add_command(label="Start {0} Preset".format(name), command= lambda: startFromFile(focus, path))
  
 def addPreset(focus):
     fileList = glob.glob(os.getcwd() + "/presets/*.json") # Iterates through json files in the directory
     for presetFile in fileList:
         try:
-            with open(presetFile, "r") as out:
-                preset = json.load(out) # Saves the data in the file to a variable
-            addOption(focus, preset, preset["subredditName"]) # Adds the option to the view tab
+            addOption(focus, presetFile, "name") # Adds the option to the view tab
         except Exception as ex:
           pass

289fd3b9f3c27d298ee94b1e79415a20ec084c4c
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-17 16:33:26 +0000

    fixed preset loading issue

--- videomaker/functions/addPreset.py
+++ videomaker/functions/addPreset.py
@@ -1,5 +1,5 @@
 from videomaker.functions.startFromFile import startFromFile
-import glob, os, threading
+import glob, os, threading, json
 
 def addOption(focus, path, name="Undefined"):
     focus.menuDropdownStartFromFile.add_command(label="Start {0} Preset".format(name), command= lambda: startFromFile(focus, path))
@@ -7,7 +7,11 @@ def addOption(focus, path, name="Undefined"):
 def addPreset(focus):
     fileList = glob.glob(os.getcwd() + "/presets/*.json") # Iterates through json files in the directory
     for presetFile in fileList:
+        print(presetFile)
         try:
-            addOption(focus, presetFile, "name") # Adds the option to the view tab
+            with open(presetFile, "r") as out:
+                data = json.load(out)
+            addOption(focus, presetFile, data["subredditName"]) # Adds the option to the view tab
         except Exception as ex:
-          pass
+            print(ex)
+            pass
